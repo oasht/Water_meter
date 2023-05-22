6e1a475ae8402444a88116e2accbf3a08ed4d149
Olga Shtoppel <olgashtoppel@gmail.com>
2023-05-22 19:43:15 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Water_meter
+{
+    internal class Meter
+    {
+
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public DateTime CheckDate { get; set; }
+        public bool Temp { get; set; }
+    }
+}
